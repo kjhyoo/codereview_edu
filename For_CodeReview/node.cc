c631b9246ce5a5732c02ef33cb43876290382f81
Jaehyun Yoo <sjh.yoo@samsung.com>
2018-08-14 14:22:04 +0900

    Code Review 실습을 위한 코드
    Signed-off-by: Jaehyun Yoo <sjh.yoo@samsung.com>

---
+++ For_CodeReview/node.cc
@@ -0,0 +1,75 @@
+// Copyright 2018 sjh.yoo
+
+#include "node.h"
+
+Node::Node(char data) {
+  this->data = data;
+  previousNode = nullptr;
+  nextNode = nullptr;
+}
+
+char Node::GetData() {
+  return data;
+}
+
+Node* Node::GetPreviousNode() {
+  if (previousNode == nullptr) return nullptr;
+  return previousNode;
+}
+
+Node* Node::GetNextNode() {
+  if (nextNode == nullptr) return nullptr;
+  return nextNode;
+}
+
+Node* Node::InsertPreviousNode(char data) {
+  Node* newNode = new Node(data);
+  newNode->previousNode = previousNode;
+  newNode->nextNode = this;
+  
+  if (previousNode != nullptr) {
+    previousNode->nextNode = newNode;
+  }
+  previousNode = newNode;
+
+  return newNode;
+}
+
+Node* Node::InsertNextNode(char data) {
+  Node* newNode = new Node(data);
+  newNode->previousNode = this;
+  newNode->nextNode = nextNode;
+
+  if (nextNode != nullptr) {
+    nextNode->previousNode = newNode;
+  }
+  nextNode = newNode;
+
+  return newNode;
+}
+
+bool Node::ErasePreviousNode() {
+  if (previousNode == nullptr) return false;
+
+  Node* deleteNode = previousNode;
+  if (deleteNode->previousNode != nullptr) {
+    deleteNode->previousNode->nextNode = this;
+  }
+  previousNode = deleteNode->previousNode;
+  delete deleteNode;
+
+  return true;
+}
+
+bool Node::EraseNextNode() {
+  if (nextNode == nullptr) return false;
+
+  Node* deleteNode = nextNode;
+  if (deleteNode->nextNode != nullptr) {
+	  deleteNode->nextNode->previousNode = this;
+  }
+  nextNode = deleteNode->nextNode;
+  delete deleteNode;
+
+  return true;
+}
